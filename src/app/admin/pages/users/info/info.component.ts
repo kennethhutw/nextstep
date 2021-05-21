e070137283f291b261008f2702dea811469fb744
Hu Chia Wei <kenneth_hu@livemail.tw>
2021-05-21 22:17:37 +0800

    add user Info

--- src/app/admin/pages/users/info/info.component.ts
+++ src/app/admin/pages/users/info/info.component.ts
@@ -9,17 +9,20 @@ import {
   AuthStore,
   ToastService,
   EmailService,
-  PendingEditionService
+  PendingEditionService,
+  ArtistService
 } from '../../../../_services';
-
+import { TranslateService } from "@ngx-translate/core";
+import { Utility } from "src/app/_helpers";
 import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanActivateChild,
-  Router,
-  RouterStateSnapshot,
+
   ActivatedRoute
 } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup
+} from "@angular/forms";
+import { NewArtist } from "./../../../../_models";
 
 import { Observable, Subscription } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
@@ -35,14 +38,25 @@ export class UserInfoComponent implements OnInit, OnDestroy {
   editedUser: any;
   editions: any = [];
   currentUser: any;
+
+  profileImage: any;
+  profileImageFile: any;
+
+  profileForm: FormGroup;
+  IsUpdateFailed = false;
+  informMsg = null;
   constructor(
+    private translateSrv: TranslateService,
+    private artistSrv: ArtistService,
     private authStoreSrv: AuthStore,
     private pendingEditionSrv: PendingEditionService,
     private emailSrv: EmailService,
     private toastSrv: ToastService,
     private route: ActivatedRoute,
     private authSrv: AuthStore,
-    private userSrv: UserService
+    private formBuilder: FormBuilder,
+    private userSrv: UserService,
+    private utility: Utility,
   ) { }
 
   ngOnInit() {
@@ -57,7 +71,26 @@ export class UserInfoComponent implements OnInit, OnDestroy {
           this.editedUser = res["data"];
           if (!!this.editedUser) {
             this.initArtworks(this.editedUser.id);
+
+            let _location = this.editedUser.country;
+            if (this.utility.IsNullOrEmpty(_location)) {
+              _location = "";
+            }
+            this.profileForm.setValue({
+              name: this.editedUser.name,
+              bio: this.editedUser.bio,
+              location: _location,
+              website: this.editedUser.website,
+              facebook: this.editedUser.facebook,
+              twitter: this.editedUser.twitter,
+              instagram: this.editedUser.instagram,
+              tags: this.editedUser.tags
+            });
+            if (!this.utility.IsNullOrEmpty(this.editedUser.imageUrl)) {
+              this.profileImage = environment.assetUrl + this.editedUser.imageUrl;
+            }
           }
+
           //tags: "bizarre,love,romantic"
         } else {
 
@@ -68,10 +101,43 @@ export class UserInfoComponent implements OnInit, OnDestroy {
       });
 
     })
+
+    this.profileForm = this.formBuilder.group({
+      name: [""],
+      bio: [""],
+      location: [""],
+      website: [""],
+      facebook: [""],
+      twitter: [""],
+      instagram: [""],
+      tags: [""]
+    });
+
   }
 
   ngAfterViewInit(): void {
 
+  }
+  onRemoveImg(event) {
+    this.profileImage = null;
+    this.profileImageFile = null;
+  }
+
+  onDetectImage(event) {
+    if (event.target.files.length === 0)
+      return;
+
+    var mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    var reader = new FileReader();
+    this.profileImageFile = event.target.files[0];
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event) => {
+      this.profileImage = reader.result;
+    }
   }
 
   CheckStatus(value) {
@@ -189,4 +255,43 @@ export class UserInfoComponent implements OnInit, OnDestroy {
       });
   }
 
+
+  onSubmit() {
+    this.informMsg = "";
+    this.IsUpdateFailed = false;
+    let newArtist = new NewArtist();
+    newArtist.name = this.profileForm.value.name;
+    newArtist.bio = this.profileForm.value.bio;
+    newArtist.location = this.profileForm.value.location;
+    newArtist.facebook = this.profileForm.value.facebook;
+    newArtist.website = this.profileForm.value.website;
+    newArtist.twitter = this.profileForm.value.twitter;
+    newArtist.instagram = this.profileForm.value.instagram;
+    newArtist.tags = this.profileForm.value.tags;
+
+    let formData = new FormData();
+    formData.append("id", this.currentUser.id);
+    formData.append("uid", this.currentUser.id);
+    formData.append("name", newArtist.name);
+    formData.append("bio", newArtist.bio);
+    formData.append("facebook", newArtist.facebook);
+    formData.append("location", newArtist.location);
+    formData.append("website", newArtist.website);
+    formData.append("twitter", newArtist.twitter);
+    formData.append("instagram", newArtist.instagram);
+    formData.append("tags", newArtist.tags);
+    formData.append("profileImage", this.profileImageFile);
+
+    this.artistSrv.updateArtistBasicInfo(formData).subscribe(res => {
+      if (res["result"] === "successful") {
+        this.informMsg = "save successful";
+      }
+      else {
+        this.informMsg = "save failed";
+      }
+    }, error => {
+      this.informMsg = "save failed";
+      console.error("update Basic infor failed", error);
+    });
+  }
 }
