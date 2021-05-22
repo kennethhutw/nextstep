9f96edfda65e49de34e46986d441e6ca593e96b6
Hu Chia Wei <kenneth_hu@livemail.tw>
2021-05-22 10:44:21 +0800

    update domain name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://server.formosa.finance:5443',
-  assetUrl: 'https://server.formosa.finance:5443',
-  assetAPIUrl: 'https://server.formosa.finance:5443',
+  apiUrl: 'https://host.formosa.finance:5443',
+  assetUrl: 'https://host.formosa.finance:5443',
+  assetAPIUrl: 'https://host.formosa.finance:5443',
   environment: 'production',
   contractAddress: '0xFc3a6059ddd3cED1b59df596C49f83244A4f64f7'
 };
