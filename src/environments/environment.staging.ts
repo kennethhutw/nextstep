a5e3900b26e18a40582f7ddad55306323c3115d4
Hu Chia Wei <kenneth_hu@livemail.tw>
2021-05-25 03:21:08 +0800

    remove ssl

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://server.formosa.finance:52584',
-    assetUrl: 'https://server.formosa.finance:52584',
-    assetAPIUrl: 'https://server.formosa.finance:52584',
+    apiUrl: 'http://server.formosa.finance:52584',
+    assetUrl: 'http://server.formosa.finance:52584',
+    assetAPIUrl: 'http://server.formosa.finance:52584',
     environment: 'staging',
     contractAddress: '0xFc3a6059ddd3cED1b59df596C49f83244A4f64f7'
     //0xF52D3DBfB49a3045189E8C28196E493d73C6cFA3
