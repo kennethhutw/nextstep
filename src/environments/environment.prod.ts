c668fe35ecb28f47dd05b12e574620cf6e5c1a1a
Hu Chia Wei <kenneth_hu@livemail.tw>
2021-06-02 04:06:48 +0800

    change port and add hotjar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://host.formosa.finance:5443',
-  assetUrl: 'https://host.formosa.finance:5443',
-  assetAPIUrl: 'https://host.formosa.finance:5443',
+  apiUrl: 'https://host.formosa.finance',
+  assetUrl: 'https://host.formosa.finance',
+  assetAPIUrl: 'https://host.formosa.finance',
   environment: 'production',
   contractAddress: '0xb84735160528d1884bb99261ba91f88a92701008'
 };
