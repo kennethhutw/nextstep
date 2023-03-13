d7a08743dd0fcc46426712829433078e1f767e7b
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-03-13 14:45:13 +0800

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://next.airland.io:8443',
-  assetUrl: 'https://next.airland.io:8443',
-  assetAPIUrl: 'https://next.airland.io:8443',
+  apiUrl: 'https://host.airland.io:8443',
+  assetUrl: 'hhttps://host.airland.io:8443',
+  assetAPIUrl: 'https://host.airland.io:8443',
   environment: 'production',
   contractAddress: '0xb84735160528d1884bb99261ba91f88a92701008',
   gtag: 'G-3T59MY55H9'
