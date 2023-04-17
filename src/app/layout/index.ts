59435fa5bc87641801328efe52ed4e0522eb2a79
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-04-17 23:19:55 +0800

    fixed sidebar and radio-item

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -1,5 +1,5 @@
 
-import { FindLayoutComponent } from "./findlayout/find-layout.component";
+
 import { AdminLayoutComponent } from "./adminlayout/admin-layout.component";
 import { MainLayoutComponent } from "./mainlayout/main-layout.component";
 import { SettingLayoutComponent } from "./settinglayout/setting-layout.component";
@@ -7,13 +7,13 @@ import { ProjectLayoutComponent } from './projectlayout/project-layout.component
 export const layouts = [
     ProjectLayoutComponent,
     SettingLayoutComponent,
-    FindLayoutComponent,
+
     AdminLayoutComponent,
     MainLayoutComponent
 ];
 
 
-export { FindLayoutComponent } from "./findlayout/find-layout.component";
+
 export { AdminLayoutComponent } from "./adminlayout/admin-layout.component";
 export { MainLayoutComponent } from "./mainlayout/main-layout.component";
 export { SettingLayoutComponent } from "./settinglayout/setting-layout.component";
