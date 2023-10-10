debcac7a01526e31e7403622d67b42c5ad0b87a0
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-10-10 14:10:45 +0800

    fixed wrong assetURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://host.airland.io:8443',
-  assetUrl: 'hhttps://host.airland.io:8443',
+  assetUrl: 'https://host.airland.io:8443',
   assetAPIUrl: 'https://host.airland.io:8443',
   environment: 'production',
   contractAddress: '0xb84735160528d1884bb99261ba91f88a92701008',
