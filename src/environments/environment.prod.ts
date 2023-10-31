79435f74d0104dce06a177c9622ed30496e3ef7b
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-11-01 06:19:39 +0800

    add force

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://host.airland.io:8443',
   assetUrl: 'https://host.airland.io:8443',
-  assetAPIUrl: 'https://host.airland.io:8443',
   environment: 'production',
-  contractAddress: '0xb84735160528d1884bb99261ba91f88a92701008',
   gtag: 'G-3T59MY55H9'
 };
 5
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,9 +2,7 @@ export const environment = {
     production: true,
     apiUrl: 'https://next.airland.io:8443',
     assetUrl: 'https://next.airland.io:8443',
-    assetAPIUrl: 'https://next.airland.io:8443',
     environment: 'staging',
-    contractAddress: '0xFc3a6059ddd3cED1b59df596C49f83244A4f64f7',
     gtag: 'G-3T59MY55H9'
     //0xF52D3DBfB49a3045189E8C28196E493d73C6cFA3
     //0x7A22b19cC0e81c22af561f41036E193a15F736b3
