9bcdfa4e9276297080ea1bbea95e71d5c20ceea4
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-12-03 21:20:52 +0800

    add projects

--- src/app/website/signup/info/info.component.ts
+++ src/app/website/signup/info/info.component.ts
@@ -6,7 +6,11 @@ import {
 import { TranslateService } from "@ngx-translate/core";
 import {
   UserService,
-  AppSettingsService, DataService
+  AppSettingsService,
+  DataService,
+  ActivityService,
+  ProjectService,
+  NotificationService
 } from "../../../_services";
 import {
   FormBuilder,
@@ -40,14 +44,24 @@ export class SignupInfoComponent implements OnInit {
   isMentor = false;
 
   profileForm: FormGroup;
+  projectForm: FormGroup;
 
   skillOptions: any[] = [];
   name: string = "";
   submitted = false;
 
+  pro_submitted = false;
+
   wordCount: any;
   words = 1;
   @ViewChild("text") text: ElementRef;
+
+  msg = {
+    project: "",
+    create: "",
+    bio_template_statment: "",
+    template_statment: ""
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -57,18 +71,23 @@ export class SignupInfoComponent implements OnInit {
     private dataSrv: DataService,
     private userSrv: UserService,
     private translateSrv: TranslateService,
+    private activitySrv: ActivityService,
+    private projectSrv: ProjectService,
     private appSettingsSrv: AppSettingsService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private notificationSrv: NotificationService
   ) {
 
     this.skillOptions = this.appSettingsSrv.skillOptions();
     let _lang = localStorage.getItem("lang");
     if (!this.utilitySrv.IsNullOrEmpty(_lang)) {
       this.translateSrv.use(_lang);
+      this.init_terms();
     }
     this.dataSrv.langKey.subscribe((lang) => {
       if (!this.utilitySrv.IsNullOrEmpty(lang)) {
         this.translateSrv.use(lang);
+        this.init_terms();
       }
     });
   }
@@ -81,7 +100,7 @@ export class SignupInfoComponent implements OnInit {
       name: ["", [Validators.required, Validators.pattern('^[a-zA-Z 0-9]*$')]],
       position: ["", [Validators.required, Validators.pattern('^[a-zA-Z 0-9]*$')]],
       company: [""],
-      bio: ["", Validators.maxLength(300)],
+      bio: ["", Validators.maxLength(1000)],
       skills: [""],
       website: [""],
       github: [""],
@@ -91,9 +110,34 @@ export class SignupInfoComponent implements OnInit {
       tags: [""],
 
     });
+
+    this.projectForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      description: [""],
+      isFindPartner: [0, Validators.required],
+      isFunding: [0, Validators.required],
+      isCofounder: [0, Validators.required],
+      product: [""],
+      type: ["", Validators.required],
+      stages: ["", Validators.required]
+    });
   }
 
+  init_terms() {
+    this.translateSrv.get("PROJECT").subscribe((text: string) => {
+      this.msg.project = text;
+    });
+    this.translateSrv.get("CREATE").subscribe((text: string) => {
+      this.msg.create = text;
+    });
+    this.translateSrv.get("PROJECT_INTRO_STATEMENT").subscribe((text: string) => {
+      this.msg.template_statment = text;
+    });
+    this.translateSrv.get("BIO_STATEMENT").subscribe((text: string) => {
+      this.msg.bio_template_statment = text;
+    });
 
+  }
   get f() {
     return this.profileForm.controls;
   }
@@ -174,7 +218,8 @@ export class SignupInfoComponent implements OnInit {
       this.userSrv.updateUserBasicInfo(formData).subscribe(res => {
 
         if (res['result'] == 'successful') {
-          this.router.navigate(["./dashboard"], {});
+          // this.router.navigate(["./dashboard"], {});
+          this.step = 2;
         }
       })
     } catch (error) {
@@ -190,11 +235,13 @@ export class SignupInfoComponent implements OnInit {
   }
 
   NextStep(value) {
-
+    console.log("=========", value);
     this.submitted = true;
+
     if (this.profileForm.invalid) {
       return;
     }
+
     this.step = value;
     if (this.step === 1) {
       this.name = this.profileForm.value.name;
@@ -221,5 +268,150 @@ export class SignupInfoComponent implements OnInit {
   onRemoveImg(event) {
     this.profileImage = null;
     this.profileImageFile = null;
+  }
+
+  onBioTemplate() {
+    this.profileForm.get("bio").setValue(this.msg.template_statment);
+  }
+
+  GoDashboard() {
+    this.router.navigate(["./dashboard"], {});
+  }
+
+  get g() {
+    return this.projectForm.controls;
+  }
+
+  inProjectValid() {
+    return this.projectForm.invalid;
+  }
+
+  onStatusChange($event, property) {
+    this.projectForm.get(property).setValue($event.target.checked);
+  }
+  onProductChange($event, value) {
+    var _values = this.projectForm.get('product').value;
+
+    if ($event.target.checked) {
+      _values += "," + value;
+    } else {
+      _values = _values.replace("," + value, "");
+    }
+    this.projectForm.get('product').setValue(_values);
+  }
+
+  onIndustryTypeChange($event, value) {
+
+    var _values = this.projectForm.get('type').value;
+
+    if ($event.target.checked) {
+      _values += "," + value;
+    } else {
+      _values = _values.replace("," + value, "");
+    }
+    this.projectForm.get('type').setValue(_values);
+  }
+
+  isInValue(value, types) {
+
+    if (types == "") {
+      return false;
+    } else {
+      return types.indexOf(value) > -1;
+    }
+  }
+
+  onStageChange($event, value) {
+    var _values = this.projectForm.get('stages').value;
+
+    if ($event.target.checked) {
+      if (_values.indexOf(',') > 0) {
+        _values += "," + value;
+      } else {
+        _values += value;
+      }
+    } else {
+      _values = _values.replace("," + value, "");
+      _values = _values.replace(value, "");
+    }
+    this.projectForm.get('stages').setValue(_values);
+  }
+
+  onSave() {
+
+    this.submitted = true;
+    const value = this.projectForm.value;
+    // stop here if form is invalid
+    if (this.projectForm.invalid) {
+      return;
+    }
+    this.saveProject('published', 0);
+  }
+
+  onTemplate() {
+    this.projectForm.get("description").setValue(this.msg.template_statment);
+  }
+
+  onSaveDraft() {
+    if (this.projectForm.invalid) {
+      return;
+    }
+
+    this.saveProject('draft', 0);
+  }
+
+  saveProject(status, isPublic) {
+    this.submitted = true;
+
+    const value = this.projectForm.value;
+    let projectName = value.name;
+    this.projectSrv.insertProject({
+      name: value.name,
+      description: value.description,
+      status: status,
+      product: value.product,
+      type: value.type,
+      stages: value.stages,
+      isPublic,
+      isFindPartner: value.isFindPartner,
+      isFunding: value.isFunding,
+      isCofounder: value.isCofounder,
+      uid: this.currentUser.id
+    }).subscribe(res => {
+      if (res['result'] === 'successful') {
+
+        let projectId = res['data'];
+        this.submitted = false;
+        this.activitySrv.insert(this.currentUser.id,
+          res['data'],
+          "create",
+          `${this.currentUser.name} ${this.msg.create} ${projectName} ${this.msg.project} ！`
+        ).subscribe(res => {
+          if (res['result'] === 'successful') { }
+        });
+        this.notificationSrv.insert(this.currentUser.id,
+          null,
+          ` ${this.msg.create}${projectName}${this.msg.project}！`,
+          "1",
+          '0',
+          '0',
+          this.currentUser.id
+        ).then(res => {
+          if (res['result'] === 'successful') {
+            this.GoDashboard()
+          }
+        }).catch(error => {
+          console.error("saveError", error);
+        })
+        //this.projectForm.reset();
+      } else {
+
+        console.error("saveError", res['error'].message);
+      }
+
+    }, (error) => {
+      console.error("saveError", error);
+
+    })
   }
 }
